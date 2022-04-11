4f2acf1c2279657979db402aec7683115577e469
Ankit <ankitthakur1998@gmail.com>
2022-04-11 17:20:01 +0530

    New class is HappyFortuneService

--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -11,7 +11,14 @@ public class HelloSpringApp {
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		//retrieve bean from spring container
+		
+		//BaseballCoach theCoach = new BaseballCoach();
+				//Coach theCoach = new BaseballCoach();
 		Coach theCoach = context.getBean("myCoach", Coach.class); //myCoach id is linked to TrackCoach class
+		
+       //Coach.class matlab, Coach Interface mein jo method hai, wo kis class pe apply hoga.
+		//class name ko particular naa karke, general kardiya hai, aur class ke package ko
+		//applicationContext.xml mein likh diya hai.
 		
 		//call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
