1811c6641657d022a05d6c65986e532fd091517d
Ankit <ankitthakur1998@gmail.com>
2022-04-12 14:07:04 +0530

    Literals is successfully injected

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -5,6 +5,30 @@ public class CricketCoach implements Coach {
 	//Defining private field for dependency
 	private FortuneService fortuneService;
 	
+	//add new fields for emailaddress & team
+	private String emailAddress;
+	private String team;
+	
+	public String getEmailAddress() {  //read only method
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {  //Write only method
+		System.out.println("CricketCoach: Inside Setter Method- setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {      //read only method
+		return team;
+	}
+
+	public void setTeam(String team) {     //Write only method
+		System.out.println("CricketCoach: Inside Setter Method- setTeam");
+		this.team = team;
+	}
+
+
+	
 	//Creating a no-argument constructor
 	public CricketCoach(){
 		System.out.println("CricketCoach: Inside No-Argument Constructor");
