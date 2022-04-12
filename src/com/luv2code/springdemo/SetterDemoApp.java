23ad453ff56326706273b598d9bd961dfb9e1d7d
Ankit <ankitthakur1998@gmail.com>
2022-04-12 11:42:44 +0530

    SetterDemoApp calling No-Arg Constructor & setterMethod & now Methods to call is mentioned in SetterDemoApp

--- src/com/luv2code/springdemo/SetterDemoApp.java
+++ src/com/luv2code/springdemo/SetterDemoApp.java
@@ -6,14 +6,17 @@ public class SetterDemoApp {
 
 	public static void main(String[] args) {
 		//Load the spring configuration file
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		//Retrieve bean from spring container
 		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
 		//call methods on the bean
 		// ... let's come back to this ...
+		System.out.println(theCoach.getDailyWorkout());
 		
+		System.out.println(theCoach.getDailyFortune());
 		
 		//close the context
 		context.close();
